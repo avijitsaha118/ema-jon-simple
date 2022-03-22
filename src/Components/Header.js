66f6fa28ba2fa0b6846660c98600d3aed3ce9cd0
avijitsaha118 <sahaavijit908@gmail.com>
2022-03-22 22:05:00 +0600

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images'
+const Header = () => {
+    return (
+        <nav>
+            <img src="" alt="" />
+        </nav>
+    );
+};
+
+export default Header;
